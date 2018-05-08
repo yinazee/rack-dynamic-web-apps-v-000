4997922d570a874fd840cecaffa99363d428b0fb
Yina Zhu <yinagit@gmail.com>
2018-05-08 16:58:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
-    binding.pry
+    # binding.pry
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
     resp.write "#{num_3}\n"
